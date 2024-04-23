9d75f9fd99a42923e38b31ef4d51badea68bd7ec
edris Mohammadi <edrismohammadi982@gmail.com>
2024-04-23 12:51:35 +0330

    Coding Header Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import routes from '../routes'
 import { useRoutes } from 'react-router-dom'
+import Header from './components/Header/Header'
 
 export default function App() {
 
@@ -8,6 +9,7 @@ export default function App() {
 
   return (
     <>
+      <Header />
       {router}
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Header.css"
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsIcon from '@mui/icons-material/Settings';
+import LanguageIcon from '@mui/icons-material/Language';
+
+export default function Header() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Dashboard Admin</span>
+                </div>
+
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsIcon />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <LanguageIcon />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <SettingsIcon />
+                    </div>
+                    <img src="Images/profile.jpg" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
