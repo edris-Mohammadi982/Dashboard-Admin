72abffcf0c7181c7de8b1671ca30ad9b4f530e1a
edris Mohammadi <edrismohammadi982@gmail.com>
2024-04-24 14:46:55 +0330

    Fix Chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { ResponsiveContainer, LineChart, Line, XAxis, CartesianGrid, Tooltip } from 'recharts'
+import "./Chart.css"
 
 export default function Chart({ titel, data, dataKey }) {
     return (
         <div className='chart'>
-            <h3 className='chartTitel'>{titel}</h3>
+            <h3 className='chartTitle'>{titel}</h3>
             <ResponsiveContainer width='100%' aspect={4}>
                 <LineChart data={data}>
                     <XAxis dataKey={"name"} stroke='#5550bd' />
-                    <Line dataKey={"Sale"} stroke='#5550bd' />
+                    <Line type={"monotone"} dataKey={"Sale"} stroke='#5550bd' />
                     <Tooltip />
                     <CartesianGrid stroke='#e0dfdf' strokeDasharray={"8"} />
                 </LineChart>
